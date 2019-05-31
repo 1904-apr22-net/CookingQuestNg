7b64ff82c65d165a531823e95fc85486621a70eb
unknown <sadikisaunders@gmail.com>
2019-05-31 10:12:28 -0500

    initial commit

---
+++ src/app/kitchen/kitchen.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kitchen',
+  templateUrl: './kitchen.component.html',
+  styleUrls: ['./kitchen.component.css']
+})
+export class KitchenComponent implements OnInit {
+  ingredient1 = "Test"
+  ingredient2 = "Test"
+  ingredient3 = "Test"
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
